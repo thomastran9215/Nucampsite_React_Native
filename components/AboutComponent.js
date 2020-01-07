a5f32d9a00918adaa60a965a82586087dfdbdfb1
thomastran9215 <thomastran@mail.com>
2020-01-06 22:06:33 -0600

    Using Redux; configured redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import { PARTNERS } from '../shared/partners';
 import { FlatList, ScrollView, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import { Card, ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+      partners: state.partners
+    };
+};
+
+function Mission() {
+    return (
+        <Card title='Our Mission'>
+            <Text style={{margin: 10}}>
+                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+            </Text>
+        </Card>
+    );
+}
 
 class About extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners : PARTNERS
-        };
-    }
 
     static navigationOptions = {
         title: 'About Us'
@@ -19,31 +29,23 @@ class About extends Component {
 
     render() {
 
-        function Mission () {
-            return (
-                    <Text>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
-            );
-        }
-
         const renderPartner = ({item}) => {
             return (
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         };
 
-        return(
+        return (
             <ScrollView>
-                <Card title="Our Mission">
-                    {Mission()}
-                </Card>
+                <Mission />
                 <Card 
                     title="Community Partners">
                     <FlatList
-                        data={this.state.partners}
+                        data={this.props.partners.partners}
                         renderItem={renderPartner}
                         keyExtractor={item => item.id.toString()}
                     />
@@ -53,6 +55,4 @@ class About extends Component {
     }
 }
 
-
-
-export default About;
+export default connect(mapStateToProps)(About);
