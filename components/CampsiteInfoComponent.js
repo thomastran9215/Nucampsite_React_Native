7c576d31fbf2f29d41064aa61521fce421105656
thomastran9215 <thomastran@mail.com>
2020-01-17 15:18:51 -0600

    Gestures Part 2; added rubber band to campsiteinfo

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -23,11 +23,18 @@ function RenderCampsite(props) {
 
     const {campsite} = props;
 
+    handleViewRef = ref => this.view = ref;
+
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            this.view.rubberBand(1000)
+            .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
+        },
         onPanResponderEnd: (e, gestureState) => {
+        
             console.log('pan responder end', gestureState);
             if (recognizeDrag(gestureState)) {
                 Alert.alert(
@@ -58,6 +65,7 @@ function RenderCampsite(props) {
                 animation='fadeInDown'
                 duration={2000} 
                 delay={1000}
+                ref={this.handleViewRef}
                 {...panResponder.panHandlers}>
                 <Card
                     featuredTitle={campsite.name}
